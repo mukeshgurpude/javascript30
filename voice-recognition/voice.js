e5b2424324f72d16b89571dbc528d12a3e31fe19
Mukesh <mukeshgurpude02@gmail.com>
2021-01-21 19:44:13 +0530

    Finished, but not tested

---
+++ voice-recognition/voice.js
@@ -0,0 +1,23 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recogniser = new SpeechRecognition();
+recogniser.interimResults = true;
+recogniser.continuous = true;
+recogniser.lang = 'en-US';
+
+let sentence = document.createElement('p');
+const notes = document.querySelector('main.notes');
+notes.appendChild(sentence);
+
+recogniser.addEventListener('result', e=>{
+    const data = e.results[0];
+    console.log(data[0].transcript)
+    sentence.textContent = data[0].transcript;
+    
+    if(data.isFinal){
+        sentence = document.createElement('p');
+        notes.appendChild(sentence);
+    }
+})
+
+recogniser.addEventListener('end', recogniser.start);
+recogniser.start();
